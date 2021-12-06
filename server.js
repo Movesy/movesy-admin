588f8dcfbd889e5ee9c758135680f750fec79360
Vida Péter <vida.peter1127@gmail.com>
2021-12-06 20:22:50 +0100

    heroku try_16_proxy_try_1_revert

--- server.js
+++ server.js
@@ -13,4 +13,5 @@ app.use(express.static(__dirname + '/dist/AdminPage'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname+'/dist/AdminPage/index.html'));
 });
+
 app.listen(process.env.PORT || 8080);
