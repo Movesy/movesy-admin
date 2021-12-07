c5e13284c44167f7d24b80cfb1c4ac1084c0238e
Vida Péter <vida.peter1127@gmail.com>
2021-12-07 12:33:00 +0100

    heroku try_16_proxy_not_working_set_last_stable_state

--- server.js
+++ server.js
@@ -1,33 +1,17 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware')
+const path = require('path');
 const app = express();
+app.use(requireHTTPS);
+app.use(express.static(__dirname + '/dist/AdminPage'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname+'/dist/AdminPage/index.html'));
+});
 
-const Webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
-/** this is custom js to help proxy in server.js*/
-const webpackConfig = require('./webpack.config.js');
-
-const compiler = Webpack(webpackConfig);
-const devServerOptions = { ...webpackConfig.devServer, open: true };
-const server = new WebpackDevServer(devServerOptions, compiler);
-
-const runServer = async () => {
-  console.log('Starting server...');
-  await server.start();
-};
-
-runServer();
-/** If you have error creating proxy <app-url> to localhost
- * Heroku internally redirect the Server port 8080 .
- *  For that reason we need to open listener  port(I used 3000 here) redirect
- through http-proxy-middleware*/
-app.use("/*", createProxyMiddleware(
-  { target: "https://movesy.herokuapp.com",
-    ws: true ,
-    changeOrigin: true,
-    secure:true,
-    router: {
-      'dev.localhost:3000': 'https://movesy.herokuapp.com',
-    },}))
-
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 8080);
